9c5d1b262ad532e1626d7c23dd738e918d2ec70c
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2025-07-30 12:37:27 +0700

    chore: add page metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-	import { waitLocale } from 'svelte-i18n'
+	import { page } from '$app/stores';
+	import { waitLocale } from 'svelte-i18n';
 	import '../app.css';
 
 	let { children } = $props();
-	
-	export async function preload() {    
-		return waitLocale()
+
+	export async function preload() {
+		return waitLocale();
 	}
 </script>
+
+<svelte:head>
+	<title>Character Cascade</title>
+	<meta name="description" content="A product of an insatiated Software Engineer's boredom" />
+</svelte:head>
 
 {@render children()}
